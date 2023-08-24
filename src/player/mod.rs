7b25a8c0fd61d1ce095f6d924e2e7064e95594bd
fruit-bird <github.ru3w9@slmail.me>
2023-08-24 14:38:42 +0100

    added conditional running if game is in the Playing state + debug print is on a timer

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -2,7 +2,10 @@ mod components;
 mod resources;
 mod systems;
 
-use bevy::prelude::*;
+use crate::game_state::AppState;
+
+use bevy::{prelude::*, time::common_conditions::on_fixed_timer};
+use std::time::Duration;
 use systems::*;
 
 pub use components::{Health, Player, PlayerBundle};
@@ -11,16 +14,21 @@ pub use systems::player_is_dead;
 
 pub const PLAYER_VELOCITY: f32 = 500.0;
 pub const SPRITE_IDX: usize = 119;
-pub const SECONDS_TO_DRAIN_1_HP: f32 = 0.05;
+pub const SECONDS_TO_DRAIN_1_HP: f32 = 0.5;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(HealthDrainConfig::new(SECONDS_TO_DRAIN_1_HP))
-            .add_systems(Startup, spawn_player)
-            .add_systems(Update, player_movement)
-            .add_systems(Update, (health_drain, debug_player_hp))
-            .add_systems(PreUpdate, player_dies);
+            .add_systems(Startup, spawn_player.run_if(in_state(AppState::Playing)))
+            .add_systems(
+                Update,
+                (player_movement, health_drain, player_dies).run_if(in_state(AppState::Playing)),
+            )
+            .add_systems(
+                FixedUpdate,
+                debug_player_hp.run_if(on_fixed_timer(Duration::from_secs(1))),
+            );
     }
 }
